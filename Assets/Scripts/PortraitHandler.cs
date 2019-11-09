f6687a4f478e7f859a01b40dd9007ecdb6bae56b
Justice Shultz <s189081@students.aie.edu>
2019-11-09 13:01:47 -0800

    Pushing a ton of shit

---
+++ Assets/Scripts/PortraitHandler.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PortraitHandler : MonoBehaviour
+{
+    public Image healthBar;
+    public GameObject ok;
+    public GameObject mad;
+    public GameObject REEE;
+    public GameObject dying;
+
+    void Update()
+    {
+        if(healthBar.fillAmount >= 0.765f)
+        {
+            ok.SetActive(true);
+            mad.SetActive(false);
+            REEE.SetActive(false);
+            dying.SetActive(false);
+        }
+        else if (healthBar.fillAmount >= 0.5f)
+        {
+            ok.SetActive(false);
+            mad.SetActive(true);
+            REEE.SetActive(false);
+            dying.SetActive(false);
+        }
+        else if (healthBar.fillAmount >= 0.17f)
+        {
+            ok.SetActive(false);
+            mad.SetActive(false);
+            REEE.SetActive(true);
+            dying.SetActive(false);
+        }
+        else
+        {
+            ok.SetActive(false);
+            mad.SetActive(false);
+            REEE.SetActive(false);
+            dying.SetActive(true);
+        }
+    }
+}
