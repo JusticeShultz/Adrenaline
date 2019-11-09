3bb88b5f6f08a77ab5568878a2e4ec125563924c
Justice Shultz <s189081@students.aie.edu>
2019-11-08 23:10:28 -0800

    Initial push of the adrenaline and enemies system

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EnemyAI : MonoBehaviour
+{
+    [Header("Stats")]
+    public float health = 100f;
+    public float mindamage = 25f;
+    public float maxdamage = 100f;
+    public float critChance = 5f;
+
+    [Header("Movement")]
+    public float movementSpeed;
+    public float maxDepth;
+    public float mMinDepth;
+
+    [Header("Fancy floaty numbers")]
+    public GameObject enemyDamageNumberPrefab;
+    public GameObject enemyCriticalDamagePrefab;
+
+    public UnityEvent onDoNormalDamage = new UnityEvent();
+    public UnityEvent onDoCriticalDamage = new UnityEvent();
+    public UnityEvent onTakeDamage = new UnityEvent();
+    public UnityEvent onDeath = new UnityEvent();
+
+    private bool died = false;
+
+    void Update()
+    {
+        //Do movement here
+
+        if(PlayerController.instance.transform.position.x < transform.position.x)
+        {
+            // Do sum movin' : )
+        }
+    }
+
+    public void InflictDamage(float amount)
+    {
+        health -= amount;
+        onTakeDamage.Invoke();
+
+        if(health <= 0 && !died)
+        {
+            Die();
+        }
+    }
+
+    public void DealDamage(float amount)
+    {
+        int roll = Random.Range(0, 100);
+
+        if (roll < critChance)
+        {
+            onDoCriticalDamage.Invoke();
+            PlayerController.instance.InflictDamage(amount * 3.5f);
+
+            TextMesh text = Instantiate(enemyCriticalDamagePrefab, transform.position + transform.forward, Quaternion.identity).GetComponent<TextMesh>();
+            text.text = Mathf.Ceil(amount * 3.5f).ToString();
+        }
+        else
+        {
+            onDoNormalDamage.Invoke();
+            PlayerController.instance.InflictDamage(amount);
+
+            TextMesh text = Instantiate(enemyDamageNumberPrefab, transform.position + transform.forward, Quaternion.identity).GetComponent<TextMesh>();
+            text.text = Mathf.Ceil(amount * 3.5f).ToString();
+        }
+    }
+
+    public void Die()
+    {
+        died = true;
+        onDeath.Invoke();
+    }
+
+    IEnumerator TextAnimation(TextMesh textObject)
+    {
+        for (int i = 0; i < 50; i++)
+        {
+            yield return new WaitForSeconds(0.01f);
+            textObject.transform.position += new Vector3(0, 0.005f, 0);
+        }
+
+        Destroy(textObject.gameObject);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,115 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static PlayerController instance;
+
+    [Header("Health")]
+    public float maxHealth = 1000;
+    public float currentHealth = 250;
+    public float regenerationIntensity = 4;
+    public float standingStillRegenTime = 2f;
+    public float noDamageRegenTime = 2.5f;
+    public Image HealthSlider;
+
+    [Header("Movement")]
+    public float movementSpeed = 100f;
+    public float maxDepth = 10f;
+    public float minDepth = -5f;
+    public Rigidbody rb;
+
+    [Header("Damage")]
+    public float attackCooldown = 0.3f;
+    public float currentDamage = 0f;
+
+    [Header("Event hookups")]
+    public UnityEvent onStandStillRegen = new UnityEvent();
+    public UnityEvent onNoDamageRegen = new UnityEvent();
+    public UnityEvent onTakeDamage = new UnityEvent();
+    public UnityEvent onDeath = new UnityEvent();
+
+    private Vector3 lastPos;
+    private float standStillTime = 0f;
+    private float sinceDamageTime = 0f;
+    private float sinceAttackTime = 0f;
+    private bool Died = false;
+
     void Start()
     {
-        
+        instance = this;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //Get the difference between the max and current health and use that as a power to the value 2.
+        //Case examples:
+        //2 ^ ((1000 - 1000) * 0.01f) = 1
+        //2 ^ ((1000 - 800) * 0.01f) = 4
+        //2 ^ ((1000 - 500) * 0.01f) = 32
+        //2 ^ ((1000 - 250) * 0.01f) = 181
+        //2 ^ ((1000 - 150) * 0.01f) = 362
+        //2 ^ ((1000 - 50) * 0.01f) = 724
+
+        //Final results will be multiplied by 8
+
+        currentDamage = Mathf.Pow(2, ((maxHealth - currentHealth) * 0.01f));
+
+        if (currentHealth <= 0 && !Died) Die();
+
+        sinceDamageTime += Time.deltaTime;
+
+        if (transform.position == lastPos)
+            standStillTime += Time.deltaTime; 
+        else standStillTime = 0f;
+
+        if(Input.GetMouseButtonDown(0))
+        {
+
+        }
+
+        HealthSlider.fillAmount = Mathf.Lerp(HealthSlider.fillAmount, currentHealth / maxHealth, 0.04f);
+
+        //rb.velocity = Vector3.zero;
+
+        //Vector3 forwardVector = transform.forward;
+        //Vector3 movementVector = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        //movementVector = Vector3.Normalize(movementVector);
+        //rb.AddForce(movementVector * Time.deltaTime * MovementSpeed);
+
+        //transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, MinY, MaxY), transform.position.z);
+
+        if (standStillTime >= standingStillRegenTime)
+        {
+            onStandStillRegen.Invoke();
+            currentHealth = Mathf.Clamp(currentHealth + regenerationIntensity, 0, maxHealth);
+        }
+
+        if (sinceDamageTime >= noDamageRegenTime)
+        {
+            onNoDamageRegen.Invoke();
+            currentHealth = Mathf.Clamp(currentHealth + regenerationIntensity, 0, maxHealth);
+        }
+
+        lastPos = transform.position;
+    }
+
+    public void InflictDamage(float damageAmount)
+    {
+        currentHealth -= damageAmount;
+        sinceDamageTime = 0f;
+
+        onTakeDamage.Invoke();
+        //Do hit effect here
+        //Update healthbar here
+    }
+
+    public void Die()
+    {
+        Died = true;
+        onDeath.Invoke();
     }
 }
