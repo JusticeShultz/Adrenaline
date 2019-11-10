ccec92137300d64344275e3bf59cc810def04dca
Justice Shultz <s189081@students.aie.edu>
2019-11-09 23:30:35 -0800

    Lightninnnngggggg AND ENEMIES WITH WEAPONNSSSSSS~~~

---
+++ Assets/Scripts/UVScroll.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class UVScroll : MonoBehaviour
+{
+    // Scroll main texture based on time
+
+    public float scrollSpeedX = 0.5f;
+    public float scrollSpeedY = 0.5f;
+    Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        float offsetx = Time.time * scrollSpeedX;
+        float offsety = Time.time * scrollSpeedY;
+        rend.material.SetTextureOffset("_MainTex", new Vector2(offsetx, offsety));
+    }
+}
